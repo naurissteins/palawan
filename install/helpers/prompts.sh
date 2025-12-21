45230c080646894af32c6b875a31434bd9ec55d6
naurissteins <me@naurissteins.com>
2025-12-21 21:48:45 +0200

    Updated logging

--- install/helpers/prompts.sh
+++ install/helpers/prompts.sh
@@ -2,7 +2,7 @@
 ask_yes_no() {
     while true;
  do
-        read -p "$1 [y/n]: " yn
+        read -p "$1 [y/n]: " yn </dev/tty
         case $yn in
             [Yy]* ) return 0;; # Yes
             [Nn]* ) return 1;; # No
