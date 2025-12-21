c7b2b00a79d9ddc00ea19f2053c7cd257efcd023
naurissteins <me@naurissteins.com>
2025-12-21 22:26:12 +0200

    Added decoration for installing base packages

--- install/packages/base.sh
+++ install/packages/base.sh
@@ -1,3 +1,4 @@
 # Install all base packages
+print_section "Installing base packages"
 mapfile -t packages < <(grep -v '^#' "$PALAWAN_INSTALL/palawan-base.packages" | grep -v '^$')
 sudo pacman -S --noconfirm --needed "${packages[@]}"
