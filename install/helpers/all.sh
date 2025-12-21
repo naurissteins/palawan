8ae87a8b2c59a1bed3fa9316b17fb865b3b6b5a0
naurissteins <me@naurissteins.com>
2025-12-21 20:46:36 +0200

    Update 3

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -1,4 +1,4 @@
 source $PALAWAN_INSTALL/helpers/chroot.sh
-source $PALAWAN_INSTALL/helpers/logging.sh
+# source $PALAWAN_INSTALL/helpers/logging.sh
 #source $PALAWAN_INSTALL/helpers/presentation.sh
 #source $PALAWAN_INSTALL/helpers/errors.sh
